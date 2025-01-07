b1b379d6a7372628c14369fde683ddb79e07b058
half pace <halfpace64@gmail.com>
2025-01-08 01:46:40 +0530

    Codes

---
+++ app.js
@@ -0,0 +1,37 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-button");
+
+let turnO = true;
+
+const winPatterns = [ // winning patterns 
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+        console.log("box clicked");
+        if (turnO) { //player O
+            box.innerText = "O";
+            turnO = false;
+        }else { //Player X
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true; //when a box is clicked twice it overwrites the previous value so this disables it
+
+        checkWinner();
+    });
+});
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        console.log(pattern[0], pattern[1], pattern[2]);
+    }
+};
