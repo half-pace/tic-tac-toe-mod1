ba979f13d89f18a48fa8a3516883019a82a7adbd
half pace <halfpace64@gmail.com>
2025-01-08 21:13:46 +0530

    New Functionality added with reset button and New Game button

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-button");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
 
@@ -16,7 +19,7 @@ const winPatterns = [ // winning patterns
 
 boxes.forEach(box => {
     box.addEventListener("click", () => {
-        console.log("box clicked");
+        //console.log("box clicked");
         if (turnO) { //player O
             box.innerText = "O";
             turnO = false;
@@ -30,8 +33,47 @@ boxes.forEach(box => {
     });
 });
 
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerHTML = '';
+    }
+}
+
+const showWinner = () => {
+    msg.innerText = `Congratulations, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
 const checkWinner = () => {
     for (let pattern of winPatterns) {
-        console.log(pattern[0], pattern[1], pattern[2]);
-    }
+        //console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
+    
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 != "" && pos2 != "" && pos3 != ""){
+            if (pos1 == pos2 && pos2 == pos3) {
+                //console.log("Winner", pos1);
+                showWinner(pos1);
+            }
+        }
+    }    
 };
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
